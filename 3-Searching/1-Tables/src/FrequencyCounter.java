946ede9e16cac6e0c4fb52d23ebf2152691bf4c4
FeiJiHQDa <feijihqda@126.com>
2017-04-24 23:42:02 +0800

    test Mulitway

--- 3-Searching/1-Tables/src/FrequencyCounter.java
+++ 3-Searching/1-Tables/src/FrequencyCounter.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
 
 /**
  * 符号表， 基于顺序查看 顺序搜索
@@ -17,8 +18,15 @@ public class FrequencyCounter {
 
     public static void main(String[] args) {
         int minlen = Integer.parseInt(args[0]);
+        In arr = new In(args[1]);
+
 
         ST<String, Integer> st = new ST<String, Integer>();
+
+
+        while (arr.isEmpty()) {
+
+        }
 
         StdOut.print(minlen);
 
