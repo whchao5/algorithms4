d6c403f1314022d40ad4079faa7f5d7a7df55ec8
FeiJiHQDa <feijihqda@126.com>
2017-06-06 23:27:44 +0800

    update LinearProbingHashST 线性探索表

--- 3-Searching/3-4-HashTables/src/LinearProbingHashST.java
+++ 3-Searching/3-4-HashTables/src/LinearProbingHashST.java
@@ -11,12 +11,48 @@
 public class LinearProbingHashST<Key, Value> {
     private static final int INIT_CAPACITY = 4;
 
+    private int n;              // 符号表中的总数
+    private int m;              // 线性表的大小
+    private Key[] keys;         // key
+    private Value[] vals;       // value
+
 
     public LinearProbingHashST() {
         this(INIT_CAPACITY);
     }
 
-    public LinearProbingHashST(int m) {
+    public LinearProbingHashST(int capacity) {
+        this.m = capacity;
+        n = 0;
+        keys = (Key[]) new Object[m];
+        vals = (Value[]) new Object[m];
+    }
 
+    public void put(Key key, Value val) {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+
+        if (val == null) {
+            return;
+        }
+
+        if (n >= m/2) resize(m*2);
+
+
+    }
+
+    private void resize(int L) {
+
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
     }
 }
