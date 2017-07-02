b44456a9efde4da1bd6e491f7ff1e40dea6d6c48
FeiJiHQDa <feijihqda@126.com>
2017-07-02 17:16:57 +0800

    update sparseVector

--- 3-Searching/3-5-Applications/src/SparseVector.java
+++ 3-Searching/3-5-Applications/src/SparseVector.java
@@ -1,3 +1,11 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import sun.security.pkcs11.Secmod;
+
+
 /**
  * Created by W on 2017/6/25.
  * 稀疏向量 ....
@@ -5,5 +13,11 @@
  * 矩阵 和 向量的乘法
  */
 public class SparseVector {
+    private int d;
+    private ST<Integer, Double> st;
 
+    public SparseVector(int d) {
+        this.d = d;
+        this.st = new ST<Integer, Double>()    ;
+    }
 }
