b92c7e7e853b5ccb55c8077c421bb68abfdb6bc8
FeiJiHQDa <feijihqda@126.com>
2017-07-11 23:56:42 +0800

    update Graph

--- 3-Searching/3-5-Applications/src/DeDup.java
+++ 3-Searching/3-5-Applications/src/DeDup.java
@@ -1,12 +1,14 @@
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
 
 /**
  * Created by W on 2017/6/11.
  * 过滤器
  *
  * ../algs4-data/tinyTale.txt
+ * ../algs4-data/tinyG.txt
  */
 public class DeDup {
 
@@ -15,6 +17,9 @@ public class DeDup {
 
     public static void main(String[] args) {
         SET<String> set = new SET<String>();
+
+        In in = new In(args[0]);
+        StdOut.println(in.readInt());
 
         while (!StdIn.isEmpty()) {
             String key = StdIn.readAll();
--- 4-Graphs/4-1-UndirectedGraphs/src/Graph.java
+++ 4-Graphs/4-1-UndirectedGraphs/src/Graph.java
@@ -1,9 +1,11 @@
 import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
 
 /**
  * Created by W on 2017/7/6.
  * 无向图
  * Graph
+ *
  */
 public class Graph {
 
@@ -11,11 +13,20 @@ public class Graph {
     private int E;  // 边数
     private Bag<Integer>[] adj;
 
+    /*
+    ** 初始化一个空的图 顶点和边。
+     */
     public Graph(int V) {
         this.V = V;
         adj = (Bag<Integer>[]) new Bag[V];   // 创建邻接表
         for (int v = 0; v < V; v++) {
             adj[v] = new Bag<Integer>();
         }
+    }
+
+    // 初始化图形从指定的输入流。
+    public Graph(In in) {
+        this.V = in.readInt();
+
     }
 }
