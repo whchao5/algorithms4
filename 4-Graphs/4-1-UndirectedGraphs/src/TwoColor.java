c7e18bc29ab04879a3bf57c80eeda842678803b7
wuhaichao <feijihqda@126.com>
2017-09-12 10:13:47 +0800

    update code

--- 4-Graphs/4-1-UndirectedGraphs/src/TwoColor.java
+++ 4-Graphs/4-1-UndirectedGraphs/src/TwoColor.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stack;
 
 /*
 **  深度优先， ---- 双色
@@ -9,11 +10,14 @@ public class TwoColor {
 
     private boolean[] marked;
     private boolean[] colors;
-    private boolean hasTwoColor = true;
+    private int[] edgeTo;
+    private boolean hasTwoColorable = true;
+    private Stack<Integer> cycle;  // odd-length cycle
 
     public TwoColor(Graph G) {
         marked = new boolean[G.V()];
         colors = new boolean[G.V()];
+        edgeTo = new int[G.V()];
         for (int i = 0; i < G.V(); i++) {
             if (!marked[i]) {
                 dfs(G, i);
@@ -27,9 +31,19 @@ public class TwoColor {
         for (int w : G.adj(v)) {
             if (!marked[w]) {
                 colors[w] = !colors[v];
+                edgeTo[w] = v;
                 dfs(G, w);
+            } else if (colors[w] == colors[v]) {
+
+
+                this.hasTwoColorable = false;
             }
         }
     }
+
+    public boolean isHasTwoColorable() {
+        return this.hasTwoColorable;
+    }
+
 
 }
---
+++ 4-Graphs/4-2-DirectedGraphs/src/Digraph.java
@@ -0,0 +1,12 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Digraph {
+
+    private static final String NEWLINE = System.getProperty("line.separator");
+    private int V;  // 定点数
+    private int E;  // 边数
+    private Bag<Integer>[] adj;
+
+}
