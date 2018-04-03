86993720490d2051195955f4ce990b9fc8d135f4
whchao <feijihqda@126.com>
2018-04-04 00:17:25 +0800

    update Triest

--- 5-Strings/5-2-Tries/src/TrieST.java
+++ 5-Strings/5-2-Tries/src/TrieST.java
@@ -1,2 +1,31 @@
-public class TrieST {
+
+
+/**
+ * 单词查找树
+ *
+ *  ../algs4-data/shellsST.txt
+ */
+
+public class TrieST<Value> {
+
+    private static int R = 256;
+    private  Node root;
+    private int n;
+
+    private static class Node {
+        private Object val;
+        private Node[] next = new Node[R];
+    }
+
+    public TrieST() {
+    }
+
+    public Value get(String key) {
+
+    }
+
+
+    public void put(String key, String value) {
+
+    }
 }
