01681db4e024d7d41441c3337c82ab41c7331389
whchao <feijihqda@126.com>
2018-06-10 13:20:53 +0800

    update Search

---
+++ 5-Strings/src/StringSearch/Search.java
@@ -0,0 +1,46 @@
+package StringSearch;
+
+public class Search {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int search(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+
+        for (int i = 0; i <= N - M; i++) {
+            int j;
+            for (j = 0; j < M; j++) {
+                if (pat.charAt(j) != txt.charAt(i + j))  // 移位
+                    break;
+            }
+
+            if (j == M)
+                return i;
+        }
+        return N;
+    }
+
+    public int searchZero(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+
+        for (int i = 0, j = 0; i < N && j < M; i++) {
+
+            if (txt.charAt(i) == pat.charAt(j)) {
+                j++;
+            } else {
+
+                // 回退 txt
+                i -= j;
+                j = 0;
+            }
+
+            if (j == M)
+                return i - M;
+        }
+        return N;
+    }
+}
